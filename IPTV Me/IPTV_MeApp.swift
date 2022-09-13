602fe1a6f4c7f83d4017426377704d630f239c0f
Swen van Zanten <info@hdsign.nl>
2022-09-13 20:15:48 +0200

    Initial Commit

---
+++ IPTV Me/IPTV_MeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPTV_MeApp.swift
+//  IPTV Me
+//
+//  Created by Swen van Zanten on 13/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct IPTV_MeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
